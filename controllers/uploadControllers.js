ad083ae17b4b9edda367efa2dc997082ddf95d6e
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2022-09-13 11:43:32 -0300

    feat(backend): implements the upload controller
    Implementation of upload controller using express-fileupload and cloudinary packages to store images
    in the cloud.

--- controllers/uploadControllers.js
+++ controllers/uploadControllers.js
@@ -1,10 +1,24 @@
+const fs = require('fs');
+const cloudinary = require('cloudinary').v2;
 const { StatusCodes } = require('http-status-codes')
+const BadRequestError = require('../errors/bad-request');
 
 const uploadProductImage = async(req, res) => {
-  return res
-  .status(StatusCodes.OK)
-  .json({
-    message: "Upload Product Image route"
+  const { file } = req.files;
+
+  if(!file.mimetype.startsWith("image")){
+    throw new BadRequestError("Invalid file type. Please, provide an image.");
+  }
+
+  const result = await cloudinary.uploader.upload(
+    file.tempFilePath, 
+    { use_filename: true, folder: 'image-uploader' },
+  )
+
+  fs.unlinkSync(file.tempFilePath);
+
+  return res.status(StatusCodes.OK).json({
+    image: { src: result.secure_url }
   })
 }
 
