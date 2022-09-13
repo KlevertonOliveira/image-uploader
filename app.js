888b5ab5999829baf48d694057f263cdc587aee5
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2022-09-13 10:51:32 -0300

    feat(backend): add cloudinary and fileupload setup in app.js

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const app = express();
 /* Imports */
 const mongoose = require('mongoose');
 
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+  secure: true
+});
+
+const fileUpload = require('express-fileupload');
+
 const notFound = require('./middleware/not-found');
 const errorHandler = require('./middleware/error-handler');
 
@@ -17,12 +27,12 @@ const productRouter = require('./routes/productRoutes');
 /* Middleware */
 app.use(express.static('./public'));
 app.use(express.json());
-
+app.use(fileUpload({ useTempFiles: true }));
 
 /* Routes */
 app.use('/api/v1/products', productRouter);
 
-// errors
+/* Errors */
 app.use(notFound);
 app.use(errorHandler);
 
